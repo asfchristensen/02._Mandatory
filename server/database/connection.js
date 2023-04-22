654dbacd5498105ba93198d852db33a256956cc6
annesofiefogh <anne2z08@stud.kea.dk>
2023-04-22 11:53:55 +0200

    Basic functionality added

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,8 +1,10 @@
+import dotenv from "dotenv/config";
+
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
 const connection = await open({
-    filename: "./user.db",
+    filename: process.env.DB_FILENAME,
     driver: sqlite3.Database
 });
 
