4246fbf442c5f14ad37db7b051b13a28f0351db4
annesofiefogh <anne2z08@stud.kea.dk>
2023-04-22 12:12:57 +0200

    Updated environment variables

--- server/database/createDatabase.js
+++ server/database/createDatabase.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv/config";
 import db from "./connection.js";
 import bcrypt from "bcrypt";
 
 const isDeleteMode = process.argv.findIndex((argument) => argument === "delete_mode") === -1 ? false : true;
+console.log(isDeleteMode);
 
 if (isDeleteMode) {
     db.exec("DROP TABLE IF EXISTS users");
@@ -20,12 +22,12 @@ db.exec(
     `
 );
 
-// DML
+// DML - seeding data into the database
 
-const adminPasswordPlainText = "admin";
+const adminPasswordPlainText = process.env.ADMIN_PASSWORD;
 const adminPasswordHashed = await bcrypt.hash(adminPasswordPlainText, 12);
 
-const guestPasswordPlainText = "guest";
+const guestPasswordPlainText = process.env.GUEST_PASSWORD;
 const guestPasswordHashed = await bcrypt.hash(guestPasswordPlainText, 12);
 
 if (isDeleteMode) {
